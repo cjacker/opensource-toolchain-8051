a9ee232ee1ec22ebabe957dcd625e1637989a6e1
Cjacker <jianzhong.huang@i-soft.com.cn>
2022-05-24 23:17:08 +0800

    add N76E885 blink sample

--- blink/main.c
+++ blink/main.c
@@ -5,23 +5,35 @@
 #if defined(STC89)
   #include <stc51.h>
   #define LED P34
+
 #elif defined(STC8H)
   #include <stc8h.h>
   #define LED P34
+
 #elif defined(C8051F120)
   #include <C8051F120.h>
   #define LED P3_4
+
 #elif defined(C8051F320)
   #include <C8051F320.h>
   #define LED P0_6
+
+#elif defined(N76E003)
+  #include <n76e003.h>
+  #define LED P12
+
 #elif defined(N76E616)
   // my 616 dev board have 2 leds on board.
   #include <n76e616.h>
   #define LED P21
   #define LED1 P22
-#elif defined(N76E003)
-  #include <n76e003.h>
-  #define LED P12
+
+#elif defined(N76E885)
+  // NuTiny board have 2 les.
+  #include <n76e885.h>
+  #define LED P03
+  #define LED1 P04
+
 #elif defined(CH552)
   #include <ch552.h>
   #define LED P32
@@ -64,18 +76,21 @@ void main()
   // set P0_6 to Push-Pull
   P0MDOUT = 0x40;
 #elif defined(N76E003)
-  P1M1 = 0;
-  P1M2 = 0;
+  P1M1 = 0x00;
+  P1M2 = 0x00;
 #elif defined(N76E616)
-  P2M1 = 0;
-  P2M2 = 0;
+  P2M1 = 0x00;
+  P2M2 = 0x00;
+#elif defined(N76E885)
+  P0M1 = 0x00;
+  P0M2 = 0x00;
 #endif
  
   while(1) {
     LED = !LED;
-#if defined(N76E616)
+#if defined(N76E616) || defined(N76E885)
     LED1 = !LED1;
 #endif
-    delay(500);
+    delay(1000);
   }
 }
